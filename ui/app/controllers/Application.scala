1a1d943c9bf42577b484b304f3ac09a867ac8210
James Ward <james@jamesward.org>
2014-07-30 07:18:01 -0600

    consistent plugin path

--- ui/app/controllers/Application.scala
+++ ui/app/controllers/Application.scala
@@ -278,7 +278,7 @@ object Application extends Controller {
       app.config.id,
       Platform.getClientFriendlyFilename(app.config.location),
       // TODO - These should be drawn from the template itself...
-      Seq("plugins/welcome/welcome", "plugins/code/code", "plugins/compile/compile", "plugins/test/test", "plugins/run/run", "plugins/inspect/inspect", "plugins/deploy/heroku"),
+      Seq("plugins/welcome/welcome", "plugins/code/code", "plugins/compile/compile", "plugins/test/test", "plugins/run/run", "plugins/inspect/inspect", "plugins/heroku/heroku"),
       app.config.cachedName getOrElse app.config.id,
       // TODO - something less lame than exception here...
       app.templateID,
